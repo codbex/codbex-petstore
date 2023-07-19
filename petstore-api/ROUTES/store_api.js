5e46379ad4c21b240e27382e1c402ac83c84f78e
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 <tomyivanov06@gmail.com>
2023-07-19 19:45:29 +0300

    barebones of store api

--- petstore-api/ROUTES/store_api.js
+++ petstore-api/ROUTES/store_api.js
@@ -1,1 +1,26 @@
+const { Controller, Get, Post, Put, Delete } = require("http/v4/rs/decorators");
+const daoStore = require("codbex-petstore/gen/dao/Store/Store.js");
 
+
+@Controller
+class StoreApi{
+	@Post("/store/order")
+	orderPet(body, ctx){
+
+	}
+
+	@Get("/store/order/:orderId")
+	findOrderById(body, ctx){
+
+	}
+
+	@Delete("/store/order/:orderId")
+	deleteOrderById(body, ctx){
+
+	}
+
+	@Get("/store/inventory")
+	petInventoryByStatus(body, ctx){
+		
+	}
+}
