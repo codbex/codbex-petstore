eee9e081a8123adef3f261c94ca5d0b280217975
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 <tomyivanov06@gmail.com>
2023-07-23 11:19:58 +0300

    Start of store api without decorators

---
+++ petstore-api/ROUTES/store_api_decoratorless.js
@@ -0,0 +1,69 @@
+//----------------------TODO---------------------
+// Do the  oreders have statuses in the DB
+
+const http = require("http/rs");
+
+const daoStore = require("codbex-petstore/gen/dao/Store/Store.js");
+const daoPet = require("codbex-petstore/gen/dao/Pet/Pet.js");
+
+function isValidDate(dateString) {
+	const dateObj = new Date(dateString);
+	return !isNaN(dateObj) && dateObj instanceof Date;
+}
+
+const orderStatus = ['placed', 'delivered'];
+
+http.service({
+	"/store/order": {
+		"post": [{
+			"serve": (_ctx, request, response) => {
+				const body = request.getJSON();
+
+                ["petId", "quantity", "shipDate", "status"].forEach(elem => {
+                    if (!(elem in body)) {
+                        response.setStatus(400);
+                        return;
+                    }
+                });
+
+                const pet = daoPet.get(body.petid);
+
+                if (!pet) {
+                	response.println("Pet not found!")
+                    response.setStatus(404);
+                    return;
+                }
+
+                if(body.quantity < 1){
+                	response.println("Invalid Quantity");
+                    response.setStatus(400);
+                    return;
+                }
+
+                if(!isValidDate(body.shipDate)){
+                	response.println("Invalid Ship Date");
+                    response.setStatus(400);
+                    return;
+                }
+
+                if(!orderStatus.includes(body.status)){
+                	response.println("Invalid Status");
+                    response.setStatus(400);
+                    return;
+                }
+
+                newOreder = daoStore.create(body);
+
+                if (!newOrder) {
+                    response.println("Could not create the order");
+                    response.setStatus(500);
+                    return;
+                }
+			}
+
+			"catch": (_ctx, err, _request, response) => {
+				response.println(err);
+			}
+		}]
+	}
+})
